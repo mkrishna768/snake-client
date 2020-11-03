46e3b5b7677049b9691399f59c2748b7be4be9aa
Michael Krishna <michaelkrishna@hotmail.com>
2020-11-03 20:50:09 +0000

    trying out movement

--- client.js
+++ client.js
@@ -12,6 +12,7 @@ const connect = function() {
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write("Name: MVK");
+    //conn.write("Move: up");
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
